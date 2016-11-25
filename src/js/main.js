7c4854135623b741e098dbca0befb10f236f38a4
vkrotkikh <krotkikhvadim@gmail.com>
2016-11-25 19:11:52 +0200

    markup base pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
 (function($) {
     $(document).ready(function() {
-        $(".dropdown-button").dropdown();
+        $(".dropdown-button").dropdown({
+            belowOrigin: true
+        });
+        $('select').material_select();
+        $('.modal').modal({
+            opacity: '0.8',
+            ending_top: '25%'
+        });
+        $('.close-modal').click(function() {
+            $('.modal').modal('close');
+        });
     })
 })(jQuery);
