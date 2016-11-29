b2c7636a26955e2dcb760eae066f4e17ff6433e9
vkrotkikh <krotkikhvadim@gmail.com>
2016-11-29 12:11:02 +0200

    update prod

--- prod/js/main.js
+++ prod/js/main.js
@@ -5,11 +5,23 @@
         });
         $('select').material_select();
         $('.modal').modal({
-            opacity: '0.8',
-            ending_top: '25%'
+            opacity: '0.8'
         });
         $('.close-modal').click(function() {
             $('.modal').modal('close');
+        });
+        $('#tabSearch').tabs({
+            onShow: function() {
+                
+            }
+        });
+        $(".dropup-button").dropdown({
+            constrain_width: false,
+            // belowOrigin: true
+        });
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
         });
     })
 })(jQuery);
