fdf24924506bdb2afb4ac8ecd3e251fed2826496
vkrotkikh <krotkikhvadim@gmail.com>
2016-11-30 19:28:43 +0200

    markup responsive

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,27 +1,35 @@
 (function($) {
-    $(document).ready(function() {
-        $(".dropdown-button").dropdown({
-            belowOrigin: true
-        });
-        $('select').material_select();
-        $('.modal').modal({
-            opacity: '0.8'
-        });
-        $('.close-modal').click(function() {
-            $('.modal').modal('close');
-        });
-        $('#tabSearch').tabs({
-            onShow: function() {
-                
-            }
-        });
-        $(".dropup-button").dropdown({
-            constrain_width: false,
-            // belowOrigin: true
-        });
-        $('.datepicker').pickadate({
-            selectMonths: true, // Creates a dropdown to control month
-            selectYears: 15 // Creates a dropdown of 15 years to control year
-        });
-    })
+	$(document).ready(function() {
+		$(".dropdown-button").dropdown({
+			belowOrigin: true
+		});
+		$('select').material_select();
+		$('.modal').modal({
+			opacity: '0.8'
+		});
+		$('.close-modal').click(function() {
+			$('.modal').modal('close');
+		});
+		$('#tabSearch').tabs();
+		$(".dropup-button").dropdown({
+			constrain_width: false
+		});
+		$('.datepicker').pickadate({
+			selectMonths: true, // Creates a dropdown to control month
+			selectYears: 15     // Creates a dropdown of 15 years to control year
+		});
+		$(".button-collapse").sideNav();
+
+		$('#tabSearch a').click(function() {
+			if ($(this).attr('href') === '#searchJobs') {
+				$('#searchSchoolsContent').addClass('hide');
+				$('#searchJobContent').removeClass('hide');
+			}
+			if ($(this).attr('href') === '#searchSchools') {
+				$('#searchJobContent').addClass('hide');
+				$('#searchSchoolsContent').removeClass('hide');
+			}
+			
+		})
+	})
 })(jQuery);
