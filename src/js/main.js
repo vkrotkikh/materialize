76c1641468c7f125ea9d3be0854c5838b3264c85
vkrotkikh <krotkikhvadim@gmail.com>
2016-12-02 19:24:54 +0200

    markup school chapter

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,33 @@
 (function($) {
 	$(document).ready(function() {
+		// Дефолтный дропдаун появляется под кнопкой, а не накладывает на кнопку.
 		$(".dropdown-button").dropdown({
 			belowOrigin: true
 		});
+		// Инициализация селектов
 		$('select').material_select();
+		// Меняем прозрачность оверлея модального окна
 		$('.modal').modal({
 			opacity: '0.8'
 		});
+		// Закрываем модалку по клику на определенную кнопку
 		$('.close-modal').click(function() {
 			$('.modal').modal('close');
 		});
+		// Инициализация табов (/teacher/search.html). Если не нужно, просто поудалять айдишники с блоков табов.
 		$('#tabSearch').tabs();
-		$(".dropup-button").dropdown({
-			constrain_width: false
-		});
+		// $(".dropup-button").dropdown({
+		// 	constrain_width: false
+		// });
+		// Инициализация дефолтного дейтпикера
 		$('.datepicker').pickadate({
 			selectMonths: true, // Creates a dropdown to control month
 			selectYears: 15     // Creates a dropdown of 15 years to control year
 		});
+		// Инициализация кнопки, которая появляется и открывает навигацию сайта на разрешениях ниже 992 пикселей.
 		$(".button-collapse").sideNav();
 
+		// Сделал скрытие/отображение блоков к кнопкам табов
 		$('#tabSearch a').click(function() {
 			if ($(this).attr('href') === '#searchJobs') {
 				$('#searchSchoolsContent').addClass('hide');
@@ -29,7 +37,38 @@
 				$('#searchJobContent').addClass('hide');
 				$('#searchSchoolsContent').removeClass('hide');
 			}
-			
-		})
-	})
+		});
+	});
+
+	// Simple pie chart example with four series
+	var chartMain = new Chartist.Pie('.overall-performance-chart', {
+		series: [{
+			value: 56,
+			className: 'green',
+			meta: 'Meta One'
+		}, {
+			value: 278,
+			className: 'black',
+			meta: 'Meta Two'
+		}, {
+			value: 11,
+			className: 'red',
+			meta: 'Meta Three'
+		}]
+	});
+	var chartMain = new Chartist.Pie('.provided-feedback-chart', {
+		series: [{
+			value: 40,
+			className: 'green',
+			meta: 'Meta One'
+		}, {
+			value: 50,
+			className: 'blue',
+			meta: 'Meta Two'
+		}, {
+			value: 10,
+			className: 'red',
+			meta: 'Meta Three'
+		}]
+	});
 })(jQuery);
